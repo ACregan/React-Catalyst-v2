b548a93c75680d9ffd7548ae262003030ddd004a
Anthony Cregan <tony@tdcbitworks.co.uk>
2020-12-20 00:35:30 +0800

    v2.0 Official. Welcome page complete.

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,15 +4,8 @@ import Cube from './cube/cube'
 import styles from './home.module.scss'
 import SvgImg from '../common/svgIcon/svgIcon'
 import packageJson from '../../../package.json'
-import Accordion from './accordion/accordion'
 
 const Home = () => {
-  const [activeAccordion, setActiveAccordion] = useState('Introduction')
-
-  const accordionClick = (id) => {
-    id === activeAccordion ? setActiveAccordion('') : setActiveAccordion(id)
-  }
-
   return (
     <article className={styles.container}>
       <header className={styles.header}>
@@ -21,7 +14,7 @@ const Home = () => {
         </div>
         <div className={styles.headerTextContainer}>
           <h3>
-            React <strong>Catalyst</strong>
+            React <strong>Catalyst </strong>
             <sup>
               <i>
                 <small> v.</small>
@@ -40,97 +33,46 @@ const Home = () => {
           </h1>
         </div>
         <div className={styles.contentContainer}>
-          <div className={styles.leftContainer}></div>
-          <div className={styles.accordionContainer}>
-            {/* Create and AccordionWrapper component that maps the following to its children:
-                const [activeAccordion, setActiveAccordion] = useState('Introduction')
+          <div className={styles.leftContainer}>
+            <h6>Welcome</h6>
+            <p>
+              <em>
+                React <strong>Catalyst </strong>
+              </em>
+              is a fully featured web application development environment designed to enable the
+              rapid production and bundling of React.js based web applications and websites.
+            </p>
 
-                const accordionClick = (id) => {
-                  id === activeAccordion ? setActiveAccordion('') : setActiveAccordion(id)
-                }
-          
-              https://frontarm.com/james-k-nelson/passing-data-props-children/#cloning-props-onto-children
-           */}
-            <Accordion
-              summary="Introduction"
-              clickFunction={accordionClick}
-              id="Introduction"
-              activeAccordion={activeAccordion}
-            >
-              <p>
-                React Catalyst is a fully featured web application development environment designed
-                to enable the rapid production and bundling of React.js based web applications and
-                websites.
-              </p>
-            </Accordion>
-            <Accordion
-              summary="Built-in Features"
-              clickFunction={accordionClick}
-              id="Features"
-              activeAccordion={activeAccordion}
-            >
-              <p>React Router</p>
-              <p>SCSS Support</p>
-              <p>Viewport Information Helper</p>
-              <p>Optimised Production Build Process</p>
-            </Accordion>
-            <Accordion
-              summary="Getting Started"
-              clickFunction={accordionClick}
-              id="GettingStarted"
-              activeAccordion={activeAccordion}
-            >
-              <p>Requirements:</p>
-              <ul>
-                <li>Git</li>
-                <li>Node & NPM</li>
-              </ul>
-              <p>
-                Clone React Catalyst by creating a directory and running the following command:{' '}
-                <br />
-                <code>git clone whateverTrevor .</code>
-              </p>
-              <p>
-                Install the dependancies:
-                <br />
-                <code>npm install</code>
-              </p>
-              <p>
-                Start the development environment with:
-                <br />
-                <code>npm start</code>
-              </p>
-              <p>
-                Once you have written your app you can produce a build with the following command:
-                <br />
-                <code>npm run build</code>
-              </p>
-            </Accordion>
-            <Accordion
-              summary="Documentation"
-              clickFunction={accordionClick}
-              id="Documentation"
-              activeAccordion={activeAccordion}
-            >
-              <p>
-                Full Documentation of the built-in features and capabilities of React Catalyst can
-                be found here.
-              </p>
-              <p></p>
-            </Accordion>
-            <Accordion
-              summary="Add-on Modules"
-              clickFunction={accordionClick}
-              id="AddonModules"
-              activeAccordion={activeAccordion}
-            >
-              <p>
-                You can use <b>React Catalyst UI</b> to to further ease and aid the development
-                process. This library provides a number of configurable react components such as
-                Input Fields, Table components, Layout components and much more. For a full list of
-                the available components, click Here.
-              </p>
-            </Accordion>
+            <h6>Features</h6>
+            <ul>
+              <li>React Router</li>
+              <li>SCSS Support</li>
+              <li>Viewport Information Helper</li>
+              <li>Optimised Production Build Process</li>
+            </ul>
+
+            <h6>Documentation</h6>
+            <p>
+              Full Documentation of the built-in features and capabilities of React Catalyst can be
+              found here.
+            </p>
+          </div>
+          <div className={styles.rightContainer}>
+            <h6>Quick Start</h6>
+            <p>
+              To start building your application simply replace the <code>HomeContent </code>
+              component in the <code>src/app.jsx</code> file with your own components. The
+              <code>src/home/home.jsx</code> component can be deleted once replaced, it is simply
+              the code for this landing page and can be safely removed.
+            </p>
+
+            <h6>React Catalyst UI</h6>
+            <p>
+              You can use <b>React Catalyst UI</b> to to further ease and aid the development
+              process. This library provides a number of configurable react components such as Input
+              Fields, Table components, Layout components and much more. For a full list of the
+              available components, click Here.
+            </p>
           </div>
         </div>
       </main>
